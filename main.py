422c5b0f9c33904983198a70d75c65207a41906c
domusense <37619939+domusense@users.noreply.github.com>
2018-06-03 14:54:40 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,35 @@
+import falcon
+from rpi_rf import RFDevice
+
+GPIO = 17
+TX_PROTO = 1
+TX_REPEAT = 8
+TX_PULSELENGTH = None
+
+def transmit_code(code):
+    """tx RF code"""
+    # init the gpio device
+    rfdevice = RFDevice(gpio=GPIO,
+                    tx_proto=TX_PROTO,
+                    tx_pulselength = TX_PULSELENGTH,
+                    tx_repeat=TX_REPEAT)
+                    #tx_length=24,
+                    #rx_tolerance=80)
+
+    # send code
+    rfdevice.enable_tx()
+    rfdevice.tx_code(code)
+    rfdevice.cleanup()
+
+class tx(object):
+    def on_get(self, req, res,code):
+        """Handles all GET requests."""
+        res.status = falcon.HTTP_200  # ok
+        transmit_code(int(code))
+        res.body = ('Code transmitted: ' + code)
+
+# Create the Falcon application object
+app = falcon.API()
+
+# Add a route tos serve the resource
+app.add_route('/tx/{code}', tx())
