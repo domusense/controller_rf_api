64d1d89243f6b3375730b46bc4b434fa34e7ad65
domusense <37619939+domusense@users.noreply.github.com>
2018-06-03 15:49:56 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 docker build -t domu/gunicorn .
 
-docker run -d --name rf_api -e GUNICORN_WORKERS=4 -e GUNICORN_ACCESSLOG=- -p 8081:8081 domu/gunicorn 
+docker run -d --name rf_api --device /dev/gpiomem -e GUNICORN_WORKERS=4 -e GUNICORN_ACCESSLOG=- -p 8081:8081 domu/gunicorn 
