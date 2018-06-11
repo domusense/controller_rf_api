66e62272eda5001c6e71953371bca9dc8d0f0ef6
domusense <37619939+domusense@users.noreply.github.com>
2018-06-12 00:10:02 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ logger = logging.getLogger(__name__)
 
 GPIO = 22
 TX_PROTO = 1
-TX_REPEAT = 8
 TX_PULSELENGTH = None
 
 def transmit_code(code):
@@ -15,7 +14,7 @@ def transmit_code(code):
     rfdevice = RFDevice(gpio=GPIO,
                     tx_proto=TX_PROTO,
                     tx_pulselength = TX_PULSELENGTH,
-                    tx_repeat=TX_REPEAT)
+                    tx_repeat=repeats)
                     #tx_length=24,
                     #rx_tolerance=80)
 
@@ -25,7 +24,7 @@ def transmit_code(code):
     rfdevice.cleanup()
 
 class tx(object):
-    def on_get(self, req, res,code):
+    def on_get(self, req, res,code, repeats = 8):
         """Handles all GET requests."""
         res.status = falcon.HTTP_200  # ok
         transmit_code(int(code))
@@ -35,4 +34,4 @@ class tx(object):
 app = falcon.API()
 
 # Add a route tos serve the resource
-app.add_route('/tx/{code}', tx())
+app.add_route('/tx/{code}/{repeats}', tx())
