29884e0ea4b1ee3608870ae94eef9de08d307c75
domusense <37619939+domusense@users.noreply.github.com>
2018-06-16 14:24:35 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 docker build -t domu/gpio_api .
 sudo mkdir /rfapi
 
-docker run -d -v /rfapi:/gunicorn --name rfapi --device /dev/gpiomem -e GUNICORN_WORKERS=4 -e GUNICORN_ACCESSLOG=- -p 8081:8081 domu/gpio_api
+docker run -d --name rfapi --device /dev/gpiomem -e GUNICORN_WORKERS=4 -e GUNICORN_ACCESSLOG=- -p 8081:8081 domu/gpio_api
