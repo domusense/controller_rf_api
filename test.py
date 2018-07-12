cbdd1fe6520c32a39ca6f07244f0a1da870ec3eb
domusense <37619939+domusense@users.noreply.github.com>
2018-07-12 04:20:20 +0300

    Uploaded test.py
    file used to test methods without activating the API

---
+++ test.py
@@ -0,0 +1,22 @@
+import falcon
+from rpi_rf import RFDevice
+
+GPIO = 17
+TX_PROTO = 1
+TX_REPEAT = 8
+TX_PULSELENGTH = None
+
+
+# init the gpio device
+rfdevice = RFDevice(gpio=GPIO,
+	tx_proto=TX_PROTO,
+        tx_pulselength = TX_PULSELENGTH,
+        tx_repeat=TX_REPEAT)
+        #tx_length=24,
+        #rx_tolerance=80)
+
+# send code
+rfdevice.enable_tx()
+rfdevice.tx_code(123456789)
+rfdevice.cleanup()
+
