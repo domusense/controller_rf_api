d2080f4d93b730e068b40b3c9db1fb414d4bff64
domusense <37619939+domusense@users.noreply.github.com>
2018-07-14 11:10:28 +0300

    Update Signal.py

--- Signal.py
+++ Signal.py
@@ -1,1 +1,9 @@
+class Signal:
+   'Signal object'
 
+   def __init__(self, length, state):
+      self.state = state
+      self.length = length
+
+   def get_wave_form(self):
+      return wave_form
