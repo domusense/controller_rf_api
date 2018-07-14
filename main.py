66e7276feb346cc4d365b3706bbda7c9a6c7568f
Domu <git@domu.tech>
2018-07-14 15:18:58 +0000

    changed gpio number

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ import random
 from Signal import Signal
 
 TX_GPIO1 = 24
-TX_GPIO2 = 17
-TX_HIGH = "008081FF"
-TX_LOW = "000081FF"
+#TX_GPIO2 = 23
+TX_PROTO = 1
+TX_PULSELENGTH = None
 
 # show the code in stdout
 #for i in range(0, 48, 2):
@@ -20,14 +20,7 @@ TX_LOW = "000081FF"
 
 #print(2)
 
-#tx(code)
-
 logger = logging.getLogger(__name__)
-
-GPIO = 22
-TX_PROTO = 1
-TX_PULSELENGTH = None
-
 
 class Signal:
    'Signal object'
@@ -39,14 +32,20 @@ class Signal:
    def get_wave_form(self):
       return wave_form
 
-def transmit_curtain_code(pulses):
+def transmit_curtain_code(code, repeats):
+
+    TX_HIGH = "008081FF"
+    TX_LOW = "000081FF"
+
     wave_forms = [
               [Signal(800, TX_LOW),
               Signal(400, TX_HIGH)],
               [Signal(400, TX_LOW),
+              Signal(800, TX_HIGH)],
+              [Signal(6000, TX_LOW),
               Signal(800, TX_HIGH)]
-    ]
-
+          ]
+          
     square = []
 
     # init pigpio
@@ -57,15 +56,32 @@ def transmit_curtain_code(pulses):
        print("pigpio not ready")
        exit()
 
-    #build the wave pattern by pulses
-    # signal[0] - timestamp, signal[1] - GPIO state
-    for s in pulses:
-        if str(s.state.rstrip()) == "008081FF":
-            square.append(pigpio.pulse(1<<TX_GPIO, 0, s.length))
-        else:
-            square.append(pigpio.pulse(0,1<<TX_GPIOTX, s.length))
 
-    pi.set_mode(TX_GPIO, pigpio.OUTPUT) # set output mode on select GPIO
+    bin_code = '{0:08b}'.format(code)
+
+    pulses = []
+
+    # translate pulses
+    for i in range(0, len(bin_code)) :
+        #print(bin_code[i], end ='')
+        if (bin_code[i] == "0"):
+            pulses.extend(wave_forms[0])
+        elif bin_code[i] == "1":
+            pulses.extend(wave_forms[1])
+    
+    # sync-bit       
+    pulses.extend(wave_forms[2]) 
+
+    #build the wave pattern by pulses
+    print("len(pulses)=%s" % len(pulses))
+    # signal[0] - timestamp, signal[1] - GPIO state
+    for s in pulses :
+        if str(s.state.rstrip()) == TX_HIGH:
+            square.append(pigpio.pulse(1<<TX_GPIO1, 0, s.length))
+        else:
+            square.append(pigpio.pulse(0,1<<TX_GPIO1, s.length))
+
+    pi.set_mode(TX_GPIO1, pigpio.OUTPUT) # set output mode on select GPIO
     pi.wave_add_generic(square) # add the generated wave from
     wid = pi.wave_create()
     if wid >= 0:
@@ -73,13 +89,16 @@ def transmit_curtain_code(pulses):
        time.sleep(1)
        pi.wave_tx_stop()
        pi.wave_delete(wid)
-
+    
+    # stop transmission
     pi.stop()
+
+    return bin_code;
 
 def transmit_1way_code(code, repeats):
     """tx RF code"""
     # init the gpio device
-    rfdevice = RFDevice(gpio=GPIO,
+    rfdevice = RFDevice(gpio=TX_GPIO1,
                     tx_proto=TX_PROTO,
                     tx_pulselength = TX_PULSELENGTH,
                     tx_repeat = repeats)
@@ -94,7 +113,7 @@ def transmit_1way_code(code, repeats):
 def transmit_2way_code(code, repeats):
     """tx RF code"""
     # init the gpio device
-    rfdevice = RFDevice(gpio=GPIO,
+    rfdevice = RFDevice(gpio=TX_GPIO1,
                     tx_proto=TX_PROTO,
                     tx_pulselength = TX_PULSELENGTH,
                     tx_repeat = repeats)
@@ -124,8 +143,7 @@ class tx_yolanda(object):
     def on_get(self, req, res,code, repeats):
         """Handles all GET requests."""
         res.status = falcon.HTTP_200  # ok
-        transmit_curtain_code(int(code), int(repeats))
-        res.body = ('Code transmitted: ' + code)
+        res.body = ('Code transmitted: ' + transmit_curtain_code(int(code), int(repeats)))
 
 # Create the Falcon application object
 app = falcon.API()
